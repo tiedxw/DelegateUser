0724fd50c47866c2c589c8d04a166c924b9f8782
RS122\douba.shusaburo <douba.shusaburo@ricksoft.jp>
2018-09-07 15:35:39 +0900

    内容をコピーしてみたが失敗...

--- jp.ricksoft.plugins.delegate_usermanager_for_jira/src/main/java/jp/ricksoft/plugins/delegate_usermanager_for_jira/action/DelegateListener.java
+++ jp.ricksoft.plugins.delegate_usermanager_for_jira/src/main/java/jp/ricksoft/plugins/delegate_usermanager_for_jira/action/DelegateListener.java
@@ -13,22 +13,21 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.atlassian.jira.component.ComponentAccessor;
-//import com.atlassian.jira.user.util.UserManager;
 import com.atlassian.jira.security.groups.GroupManager;
 import com.atlassian.jira.user.UserPropertyManager;
 import com.atlassian.jira.issue.comments.CommentManager;
 import com.atlassian.jira.user.ApplicationUser;
-import com.opensymphony.module.propertyset.PropertySet;
 import com.atlassian.jira.util.json.JSONObject;
 import com.atlassian.jira.util.json.JSONException;
 import java.util.*;
 
-
 @Component
 public class DelegateListener implements InitializingBean, DisposableBean {
     private static final Logger log = LoggerFactory.getLogger(DelegateListener.class);
-    private static final String PUBLIC_COMMENT_KEY = "sd.public.comment";
-
+    
+	private static final String PUBLIC_COMMENT_KEY = "sd.public.comment";
+	
+	
     @JiraImport
     private final EventPublisher eventPublisher;
 
@@ -37,69 +36,7 @@ public class DelegateListener implements InitializingBean, DisposableBean {
         this.eventPublisher = eventPublisher;
     }
 
-    @EventListener
-    public void onIssueEvent(IssueEvent issueEvent) {
-        Long eventTypeId = issueEvent.getEventTypeId();
-        Issue issue = issueEvent.getIssue();
-
-        //UserManager userManager = ComponentAccessor.getUserManager();
-        GroupManager groupManager = ComponentAccessor.getGroupManager();
-        UserPropertyManager userPropertyManager = ComponentAccessor.getUserPropertyManager();
-        CommentManager commentManager = ComponentAccessor.getCommentManager();
-
-        /*
-         * 課題作成イベントのとき...
-         */
-        if (eventTypeId.equals(EventType.ISSUE_CREATED_ID)) {
-
-            /*
-             * 実際にコメントするユーザー
-             * プロジェクトリーダー
-             */
-            ApplicationUser projectLeader = issue.getProjectObject().getProjectLead();
-            ApplicationUser reporter = issue.getReporter();
-            PropertySet properties = userPropertyManager.getPropertySet(reporter);
-
-
-            /*
-             * 内部コメントプロパティの作成
-             */
-            JSONObject internalJsonObject = new JSONObject();
-            try {
-            	internalJsonObject.put("internal", true);
-            }catch(JSONException e) {
-            	//log.info(e.getStackTrace().toString());
-            }
-            Map<String, JSONObject> internalProperty = new HashMap<>();
-            internalProperty.put(PUBLIC_COMMENT_KEY, internalJsonObject);
-
-            /*
-             * グループをチェックしてコメントを作成
-             */
-            Collection<String> groups = groupManager.getGroupNamesForUser(reporter);
-            StringBuilder sb = new StringBuilder();
-            for (String group : groups) {
-                if (group.equals(DelegateManager.getGoldSupportUsresGruopName())) {
-                    sb.append("ゴールドサポートご利用です : ").append(DelegateManager.getProperties(reporter)).append("\n");
-                } else if (group.equals(DelegateManager.getSilverSupportUsresGruopName())) {
-                    sb.append("シルバーサポートご利用です : ").append(DelegateManager.getProperties(reporter)).append("\n");
-                } else if (group.equals(DelegateManager.getRickcloudUsresGruopName())) {
-                    sb.append("リッククラウドご利用です : ").append("\n");
-                } else if (group.equals(DelegateManager.getEvaluationSupportUsresGruopName())) {
-                    sb.append("評価利用です ").append("\n");
-                }
-            }
-
-            /*
-             * コメント追加
-             */
-            if (sb.toString().length() > 0) {
-            	commentManager.create(issue, projectLeader, sb.toString(), null, null, new Date(), internalProperty, true);
-            }
-        }
-    }
-	
-	/**
+    /**
      * Called when the plugin has been enabled.
      * @throws Exception
      */
@@ -117,5 +54,60 @@ public class DelegateListener implements InitializingBean, DisposableBean {
     public void destroy() throws Exception {
         log.info("Disabling plugin");
         eventPublisher.unregister(this);
+    }
+
+    @EventListener
+    public void onIssueEvent(IssueEvent issueEvent) {
+        Long eventTypeId = issueEvent.getEventTypeId();
+        Issue issue = issueEvent.getIssue();
+        CommentManager commentManager = ComponentAccessor.getCommentManager();
+        UserPropertyManager userPropertyManager = ComponentAccessor.getUserPropertyManager();
+        GroupManager groupManager = ComponentAccessor.getGroupManager();
+        
+        
+        /*
+         * 実際にコメントするユーザー
+         * プロジェクトリーダー
+         */
+        ApplicationUser projectLeader = issue.getProjectObject().getProjectLead();
+        
+        ApplicationUser reporter = issue.getReporter();
+        //PropertySet properties = userPropertyManager.getPropertySet(reporter);
+        
+        if (eventTypeId.equals(EventType.ISSUE_CREATED_ID)) {
+            
+            /*
+             * 内部コメントプロパティの作成
+             */
+            JSONObject internalJsonObject = new JSONObject();
+            try {
+            	internalJsonObject.put("internal", true);
+            }catch(JSONException e) {
+            	e.printStackTrace();
+            }
+            Map<String, JSONObject> internalProperty = new HashMap<>();
+            internalProperty.put(PUBLIC_COMMENT_KEY, internalJsonObject);
+            
+            /*
+             * グループをチェックしてコメントを作成
+             */
+            Collection<String> groups = groupManager.getGroupNamesForUser(reporter);
+            StringBuilder sb = new StringBuilder();
+            for (String group : groups) {
+                if (group.equals(DelegateManager.getGoldSupportUsresGruopName())) {
+                    sb.append("ゴールドサポートご利用です : ").append(DelegateManager.getProperties(reporter)).append("\n");
+                } else if (group.equals(DelegateManager.getSilverSupportUsresGruopName())) {
+                    sb.append("シルバーサポートご利用です : ").append(DelegateManager.getProperties(reporter)).append("\n");
+                } else if (group.equals(DelegateManager.getRickcloudUsresGruopName())) {
+                    sb.append("リッククラウドご利用です : ").append("\n");
+                } else if (group.equals(DelegateManager.getEvaluationSupportUsresGruopName())) {
+                    sb.append("評価利用です ").append("\n");
+                }
+            }
+            
+            if (sb.toString().length() > 0) {
+            	commentManager.create(issue, projectLeader, sb.toString(), null, null, new Date(), internalProperty, true);
+            }   
+        }
     }
 }
